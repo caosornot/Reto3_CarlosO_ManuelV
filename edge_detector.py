f3eba10223695dfe5491ab779ac4cb7c6a5a1d71
mvillegas1306 <mvillegasc@eafit.edu.co>
2018-05-24 18:32:32 -0500

    Scripts uptaded

--- edge_detector.py
+++ edge_detector.py
@@ -4,9 +4,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import linear_model, datasets
 import imutils
+import math
 
 #Load image
-img = cv.imread('3.jpeg')
+img = cv.imread('2.jpeg')
+
 #Convert image into HSV channel
 imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 angle = 180
@@ -17,9 +19,7 @@ imgHSV = imutils.rotate(imgHSV, angle)
 lowerBoundarie = np.array([35, 8, 0], dtype = 'uint8')
 upperBoundarie = np.array([120, 255, 255], dtype = 'uint8')
 
-#Compute mask for image
-mask = cv.inRange(imgHSV, lowerBoundarie, upperBoundarie)
-
+#Apply mask and obtain info of pixels
 pixelInfo = cv.inRange(imgHSV, np.array(lowerBoundarie), np.array(upperBoundarie))
 array = np.nonzero(pixelInfo)
 transposedArray = np.transpose(array)
@@ -39,37 +39,96 @@ y = y.reshape(-1, 1)
 # print(Y)
 
 # Fit line using all data
-lr = linear_model.LinearRegression()
-lr.fit(X, y)
+# lr = linear_model.LinearRegression()
+# lr.fit(X, y)
 
-# Robustly fit linear model with RANSAC algorithm
-ransac = linear_model.RANSACRegressor()
-ransac.fit(X, y)
-inlier_mask = ransac.inlier_mask_
-outlier_mask = np.logical_not(inlier_mask)
+# Robustly fit first linear model with RANSAC algorithm
+firstRansac = linear_model.RANSACRegressor(residual_threshold=50)
+firstRansac.fit(X, y)
+firstinlier_mask = firstRansac.inlier_mask_
+firstoutlier_mask = np.logical_not(firstinlier_mask)
 
 # Predict data of estimated models
 line_X = np.arange(X.min(), X.max())[:, np.newaxis]
-line_y = lr.predict(line_X)
-line_y_ransac = ransac.predict(line_X)
+first_line_y_ransac = firstRansac.predict(line_X)
 
-# Compare estimated coefficients
-# print("Estimated coefficients (true, linear regression, RANSAC):")
-# print(coef, lr.coef_, ransac.estimator_.coef_)
-# print(X)
+firstSlope = firstRansac.estimator_.coef_
+firstIntercept = firstRansac.estimator_.intercept_
 
+print (firstSlope)
+print(firstIntercept)
 
-lw = 2
-plt.scatter(X[inlier_mask], y[inlier_mask], color='yellowgreen', marker='.',
-            label='Inliers')
-plt.scatter(X[outlier_mask], y[outlier_mask], color='gold', marker='.',
-            label='Outliers')
-#plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
-plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=lw,
-         label='RANSAC regressor')
-plt.legend(loc='lower right')
-plt.xlabel("Input")
-plt.ylabel("Response")
-plt.show()
+# lw = 2
+# plt.scatter(X[firstinlier_mask], y[firstinlier_mask], color='yellowgreen', marker='.',
+#             label='Inliers')
+# plt.scatter(X[firstoutlier_mask], y[firstoutlier_mask], color='gold', marker='.',
+#             label='Outliers')
+# plt.plot(line_X, first_line_y_ransac, color='cornflowerblue', linewidth=lw,
+#          label='RANSAC regressor')
+# plt.legend(loc='lower right')
+# plt.xlabel("Input")
+# plt.ylabel("Response")
+# plt.show()
+
+#Delete first line
+i = 0
+deletePointsX = []
+deletePointsY = []
+
+while i<len(X): 
+
+    if abs((firstSlope*X[i])-(y[i])+(firstIntercept))/(math.sqrt((firstSlope)**2)+1) < 50:
+        deletePointsX.append(i)
+        deletePointsY.append(i)
+    i += 1
+
+X = np.delete(X, deletePointsX)
+y = np.delete(y, deletePointsY)
+
+# print(len(X))
+# print(len(y))
+
+X = X.reshape(-1, 1)
+y = y.reshape(-1, 1)
+
+# Robustly fit Second linear model with RANSAC algorithm
+secondRansac = linear_model.RANSACRegressor(residual_threshold=50)
+secondRansac.fit(X, y)
+secondinlier_mask = secondRansac.inlier_mask_
+secondoutlier_mask = np.logical_not(secondinlier_mask)
+
+# Predict data of estimated models
+line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+second_line_y_ransac = secondRansac.predict(line_X)
+
+secondSlope = secondRansac.estimator_.coef_
+secondIntercept = secondRansac.estimator_.intercept_
+
+print (secondSlope)
+print(secondIntercept)
+
+# lw = 2
+# plt.scatter(X[secondinlier_mask], y[secondinlier_mask], color='yellowgreen', marker='.',
+#             label='Inliers')
+# plt.scatter(X[secondoutlier_mask], y[secondoutlier_mask], color='gold', marker='.',
+#             label='Outliers')
+# plt.plot(line_X, first_line_y_ransac, second_line_y_ransac, color='cornflowerblue', linewidth=lw,
+#          label='RANSAC regressor')
+# plt.legend(loc='lower right')
+# plt.xlabel("Input")
+# plt.ylabel("Response")
+# plt.show()
+
+#Find intercept between first line and second line
+xIntercept = (secondIntercept-firstIntercept)/(firstSlope-secondSlope)
+xIntercept = img.shape[1]-xIntercept
+
+print (xIntercept)
+
+#Show original image with sensor
+cv.line(img, (xIntercept, 0), (xIntercept, 1000), (255, 0, 0), 5)
+cv.imshow('Line Detector', img)
+cv.waitKey(0)
+
 
 
--- reto3_manuel_carlos.py
+++ reto3_manuel_carlos.py
@@ -2,18 +2,21 @@
 import cv2 as cv
 import numpy as np 
 from matplotlib import pyplot as plt
+from sklearn import linear_model, datasets
+import imutils
+import math
 
 # Create named window
-cv.namedWindow('Line Follower')
+cv.namedWindow('Line Follower Original')
 
 # Create ON/OFF switch
 def nothing(x):
     pass
 
-cv.createTrackbar('Start - Stop', 'Line Follower', 0, 1, nothing)
-cv.createTrackbar('Quit', 'Line Follower', 0, 1, nothing)
-cv.setTrackbarPos('Start - Stop', 'Line Follower', 0)
-cv.setTrackbarPos('Quit', 'Line Follower', 0)
+cv.createTrackbar('Start - Stop', 'Line Follower Original', 0, 1, nothing)
+cv.createTrackbar('Quit', 'Line Follower Original', 0, 1, nothing)
+cv.setTrackbarPos('Start - Stop', 'Line Follower Original', 0)
+cv.setTrackbarPos('Quit', 'Line Follower Original', 0)
 # print(cv.getTrackBarPos('Quit','Line Follower'))
 
 
@@ -28,31 +31,116 @@ upperBoundarie = np.array([120, 255, 255], dtype = 'uint8')
 
 # Display initial window
 ret, frame = cap.read()
-cv.imshow('Line Follower', frame)
+cv.imshow('Line Follower Original', frame)
 
 # Loop through the video frames
 while True:
 
-    switch = cv.getTrackbarPos('Start - Stop', 'Line Follower')
+    switch = cv.getTrackbarPos('Start - Stop', 'Line Follower Original')
 
     if switch:
         ret, frame = cap.read()
         img = frame
         imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV) 
+        angle = 180+90
+        imgHSV = imutils.rotate(imgHSV, angle)
 
         #Define Color Boundaries
         lowerBoundarie = np.array([35, 8, 0], dtype = 'uint8')
         upperBoundarie = np.array([120, 255, 255], dtype = 'uint8')
-        #Compute mask for image
-        mask = cv.inRange(imgHSV, lowerBoundarie, upperBoundarie)
-        cv.imshow('Line Follower', frame)
-        cv.imshow('Line Follower2', mask)
+
+        #Apply mask and compute x & y coordinates (into a single array) of white pixels after applying the mask
+        mask = cv.inRange(imgHSV, np.array(lowerBoundarie), np.array(upperBoundarie))
+        pixelInfo = cv.inRange(imgHSV, np.array(lowerBoundarie), np.array(upperBoundarie))
+        array = np.nonzero(pixelInfo)
+        transposedArray = np.transpose(array)
+
+        #Separate x & y coordinates (different arrays)
+        X = transposedArray[: , 1]
+        y = transposedArray[: , 0]
+
+        X = X.reshape(-1, 1)
+        y = y.reshape(-1, 1)     
+
+        #Start RANSAC algorithm
+        # Robustly fit first linear model with RANSAC algorithm
+        firstRansac = linear_model.RANSACRegressor(residual_threshold=50)
+        firstRansac.fit(X, y)
+        firstinlier_mask = firstRansac.inlier_mask_
+        firstoutlier_mask = np.logical_not(firstinlier_mask)
+
+        # Predict data of estimated models
+        line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+        first_line_y_ransac = firstRansac.predict(line_X)
+
+        #Calculate and print slope and intercept for the first line.
+        firstSlope = firstRansac.estimator_.coef_
+        firstIntercept = firstRansac.estimator_.intercept_
+        print (firstSlope)
+        print(firstIntercept)
+
+        #Delete first line
+        i = 0
+        deletePointsX = []
+        deletePointsY = []
+
+        while i<len(X): 
+
+            if abs((firstSlope*X[i])-(y[i])+(firstIntercept))/(math.sqrt((firstSlope)**2)+1) < 50:
+                deletePointsX.append(i)
+                deletePointsY.append(i)
+            i += 1
+
+        X = np.delete(X, deletePointsX)
+        y = np.delete(y, deletePointsY)
+
+        X = X.reshape(-1, 1)
+        y = y.reshape(-1, 1)
+
+        # Robustly fit Second linear model with RANSAC algorithm
+        secondRansac = linear_model.RANSACRegressor(residual_threshold=50)
+        secondRansac.fit(X, y)
+        secondinlier_mask = secondRansac.inlier_mask_
+        secondoutlier_mask = np.logical_not(secondinlier_mask)
+
+        # Predict data of estimated models
+        line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+        second_line_y_ransac = secondRansac.predict(line_X)
+
+        #Calculate and print slope and intercept for the second line.
+        secondSlope = secondRansac.estimator_.coef_
+        secondIntercept = secondRansac.estimator_.intercept_
+        print (secondSlope)
+        print(secondIntercept)   
+
+        #Find intercept between first line and second line
+        xIntercept = (secondIntercept-firstIntercept)/(firstSlope-secondSlope)
+        # xIntercept = frame.shape[1]-xIntercept
+
+        print (xIntercept)   
+
+        frameRot = imutils.rotate(frame, angle)
+        cv.line(frameRot, (xIntercept, 0), (xIntercept, 1000), (255, 0, 0), 5)
+        cv.imshow('Line Follower Original', frameRot)
+        cv.line(mask, (xIntercept, 0), (xIntercept, 1000), (255, 0, 0), 5)
+        cv.imshow('Line Follower Mask', mask)
+
+        # lw = 2
+        # plt.scatter(X[inlier_mask], y[inlier_mask], color='yellowgreen', marker='.',
+        #             label='Inliers')
+        # plt.scatter(X[outlier_mask], y[outlier_mask], color='gold', marker='.',
+        #             label='Outliers')
+        # #plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
+        # plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=lw,
+        #         label='RANSAC regressor')
+        # plt.legend(loc='lower right')
+        # plt.xlabel("Input")
+        # plt.ylabel("Response")
+        # plt.show()  
 
     # print(exitApp)
     k = cv.waitKey(5) & 0xFF
     if k == 27 or not ret:
         if not ret:
             print('exit due to not ret')
-        elif exitApp:
-            print('exit due to exitApp')
         break
